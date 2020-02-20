1c0706824c418a352a3974fe449b31749838acf1
Stefan Gouldson <stefangouldson@gmail.com>
2020-02-20 12:19:22 +0000

    initial front-end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+  <div>
+    <h1>Tic Tac Toe</h1>
+
+    <table>
+
+      <tr>
+      <th>One</th>
+      <th>Two</th>
+      <th>Three</th>
+      </tr>
+
+      <tr>
+        <th>Four</th>
+        <th>Five</th>
+        <th>Six</th>
+      </tr>
+
+      <tr>
+        <th>Seven</th>
+        <th>Eight</th>
+        <th>Nine</th>
+      </tr>
+
+    </table>
+
+    <div id='button-cont'>
+    <button>Play game</button>
+    </div>
+    
+  </div>
   );
 }
 
